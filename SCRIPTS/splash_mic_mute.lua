cdcf84aeb897c9b32243e7c99fd386973ac699a7
chalwk <chalwk.dev@gmail.com>
2026-06-05 01:14:17 +1200

    Initial commit

--- SCRIPTS/splash_mic_mute.lua
+++ SCRIPTS/splash_mic_mute.lua
@@ -4,15 +4,13 @@
 local obs = obslua
 
 local SCENE_NAME = "GAMING"
-local MIC_SOURCE_NAME = "MIC/AUX"
+local MIC_SOURCE_NAME = "MIC/AUX"   -- set to "" to disable mic muting
+local DESKTOP_SOURCE_NAME = "" -- set to "" to disable desktop muting
 
-local SPLASH_SOURCES = {
-    "Starting Soon",
-    "Be Right Back",
-    "Stream Ended",
-}
+local SPLASH_SOURCES = { "Starting Soon", "Be Right Back", "Stream Ended" }
 
 local last_mic_muted = nil
+local last_desktop_muted = nil
 
 local function update_mic_mute()
     local scene_source = obs.obs_get_source_by_name(SCENE_NAME)
@@ -31,23 +29,39 @@ local function update_mic_mute()
 
     obs.obs_source_release(scene_source)
 
-    local mic_source = obs.obs_get_source_by_name(MIC_SOURCE_NAME)
-    if mic_source == nil then return end
-
     local should_mute = any_splash_visible
-    if should_mute ~= last_mic_muted then
-        last_mic_muted = should_mute
-        obs.obs_source_set_muted(mic_source, should_mute)
+
+    if MIC_SOURCE_NAME ~= "" then
+        local mic_source = obs.obs_get_source_by_name(MIC_SOURCE_NAME)
+        if mic_source then
+            if should_mute ~= last_mic_muted then
+                last_mic_muted = should_mute
+                obs.obs_source_set_muted(mic_source, should_mute)
+            end
+            obs.obs_source_release(mic_source)
+        end
+    else
+        last_mic_muted = nil
     end
 
-    obs.obs_source_release(mic_source)
+    if DESKTOP_SOURCE_NAME ~= "" then
+        local desktop_source = obs.obs_get_source_by_name(DESKTOP_SOURCE_NAME)
+        if desktop_source then
+            if should_mute ~= last_desktop_muted then
+                last_desktop_muted = should_mute
+                obs.obs_source_set_muted(desktop_source, should_mute)
+            end
+            obs.obs_source_release(desktop_source)
+        end
+    else
+        last_desktop_muted = nil
+    end
 end
 
 function script_description()
     return [[
-Mutes "MIC/AUX" while any splash screen ("Starting Soon",
-"Be Right Back", or "Stream Ended") is visible inside the
-"Splash Screens" folder of the GAMING scene.
+Mutes "MIC/AUX" and "DESKTOP" while any splash screen ("Starting Soon",
+"Be Right Back", or "Stream Ended") is visible inside the GAMING scene.
 ]]
 end
 
